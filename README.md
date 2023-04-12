60249e0bf173c4c453bd61c9b2ec42815e890ec9
Tuan Dinh <dinhngoctuan2131997@gmail.com>
2023-04-12 03:40:03 +0300

    main update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Face-Recognition-and-Liveness-Dectection
-# Face-Recognition-and-Liveness-Dectection
---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import datetime
+import pickle
+import subprocess
+
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+import face_recognition
+
+import util
+from test import test_support
+
+
+class App:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("1200x520+350+100")
+
+        self.login_button_main_window = util.get_button(self.main_window, 'Login', 'green', self.login)
+        self.login_button_main_window.place(x=750, y=200)
+
+        self.register_new_user_button_main_window = util.get_button(self.main_window, 'register new user', 'gray',
+                                                                    self.register_new_user, fg='black')
+        self.register_new_user_button_main_window.place(x=750, y=400)
+
+        self.webcam_label = util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10, y=0, width=700, height=500)
+        self.add_webcam(self.webcam_label)
+
+
+    def add_webcam(self, label):
+        if 'cap' not in self.__dict__:
+            self.cap = cv2.VideoCapture(-1)
+
+        self._label = label
+        self.process_webcam()
+
+    def process_webcam(self):
+        ret, frame = self.cap.read()
+
+        self.most_recent_capture_arr = frame
+        img_ = cv2.cvtColor(self.most_recent_capture_arr, cv2.COLOR_BGR2RGB)
+        self.most_recent_capture_pil = Image.fromarray(img_)
+        imgtk = ImageTk.PhotoImage(image=self.most_recent_capture_pil)
+        self._label.imgtk = imgtk
+        self._label.configure(image=imgtk)
+
+        self._label.after(20, self.process_webcam)
+        
+
+    def login(self):
+        pass
+    def start(self):
+        pass
+
+    def register_new_user(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
+
